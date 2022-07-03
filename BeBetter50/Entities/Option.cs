793af5b15969da4afaaf2eb98d8f7628461e5843
aragonnetje6 <twanstok@gmail.com>
2022-07-03 03:57:08 +0200

    implemented Option record class

---
+++ BeBetter50/Entities/Option.cs
@@ -0,0 +1,11 @@
+﻿namespace BeBetter50.Entities;
+
+public record Option(
+    String Name,
+    String Manufacturer,
+    int DefaultPrice,
+    int ProductionYear,
+    String Type,
+    DateTime StartDate,
+    DateTime? EndDate
+);
